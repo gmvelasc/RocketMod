031dabdcf622fbe81387f8830a6b44d9c3b179cd
gmvelasc <64888507+gmvelasc@users.noreply.github.com>
2021-05-28 18:40:46 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,6 +8,7 @@ class Menu extends Phaser.Scene {
       this.load.audio('sfx_select', './assets/blip_select12.wav');
       this.load.audio('sfx_explosion', './assets/explosion38.wav');
       this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+      //this.load.audio('splat.wav');
   }
 
   create() {
@@ -15,21 +16,21 @@ class Menu extends Phaser.Scene {
       let menuConfig = {
           fontFamily: 'Courier',
           fontSize: '28px',
-          backgroundColor: '#F3B141',
-          color: '#843605',
-          align: 'right',
+          //backgroundColor: '#B2B4C0',
+          color: '#FFFFFF',
+          align: 'center',
           padding: {
               top: 5,
               bottom: 5,
           },
           fixedWidth: 0
       }
-      
+
       // show menu text
       this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
       this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-      menuConfig.backgroundColor = '#00FF00';
-      menuConfig.color = '#000';
+      menuConfig.backgroundColor = '#B2B4C0';      
+      //menuConfig.color = '#B2B4C0';
       this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
       // define keys
