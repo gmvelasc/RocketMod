c3c47aa2deeb8d967da65df4d91b49c26d1da488
gmvelasc <64888507+gmvelasc@users.noreply.github.com>
2021-06-03 21:51:59 -0700

    Update Menu.js

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,7 +8,6 @@ class Menu extends Phaser.Scene {
       this.load.audio('sfx_select', './assets/blip_select12.wav');
       this.load.audio('sfx_explosion', './assets/explosion38.wav');
       this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
-      this.load.bitmapFont('atari', 'assets/fonts/bitmap/gem.png', 'assets/fonts/bitmap/gem.xml');
   }
 
   create() {
